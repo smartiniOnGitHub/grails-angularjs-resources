42f2244af4da9fd7f174a4aa2f25290e31f43b8a
Sandro Martini <sandro.martini@gmail.com>
2013-06-13 17:23:03 +0200

    small updates

--- test/webapp/grails-angularjs-resources-test/grails-app/controllers/grails/angularjs/resources/test/RestSampleController.groovy
+++ test/webapp/grails-angularjs-resources-test/grails-app/controllers/grails/angularjs/resources/test/RestSampleController.groovy
@@ -1,11 +1,18 @@
 package grails.angularjs.resources.test
 
+import grails.converters.JSON
+
+
 class RestSampleController 
 {
 
+	def sampleJSonResponse = [controller:'RestSampleController', method:null, message:'Hello from Grails in JSON format']
+
     def index() 
 	{
-		render(view: "index")
+		// render(view: "index")
+		// render Product.list() as JSON
+		render sampleJSonResponse as JSON
 	}
 
 }
