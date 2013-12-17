95f8172144ad2b9d3dada082161cb8583b39c209
Sandro Martini <sandro.martini@gmail.com>
2013-12-17 16:54:37 +0100

    add some improvement to the angularSampleStatic test (but still not completely working)

--- test/webapp/grails-angularjs-resources-test/grails-app/controllers/grails/angularjs/resources/test/AngularSampleStaticController.groovy
+++ test/webapp/grails-angularjs-resources-test/grails-app/controllers/grails/angularjs/resources/test/AngularSampleStaticController.groovy
@@ -9,7 +9,10 @@ class AngularSampleStaticController
 		// render(view: "page.html")  // no, it search page.html.jsp
 		// redirect(uri: "/angularSampleStatic/index.html")
 		// redirect(uri: "/angularSampleStatic/page.html")  // ok, but do not call it "index.html" ...
+		// other test: redirect to a different (and full) url, where all static resources could be hosted ...
+		//
 		redirect(uri: "/angularSampleStatic/page.html")
+
 	}
 
 }
--- test/webapp/grails-angularjs-resources-test/grails-app/controllers/grails/angularjs/resources/test/RestSampleController.groovy
+++ test/webapp/grails-angularjs-resources-test/grails-app/controllers/grails/angularjs/resources/test/RestSampleController.groovy
@@ -6,7 +6,15 @@ import grails.converters.JSON
 class RestSampleController 
 {
 
-	def sampleJSonResponse = [controller:'RestSampleController', method:null, message:'Hello from Grails in JSON format']
+	def sampleJSonResponse = [
+		id:1, 
+		controller:'RestSampleController', 
+		method:null, 
+		message:'Hello from Grails in JSON format', 
+		timestamp: new java.util.Date().format("yyyy-MM-dd HH:mm:ss.SSS"), 
+		// timestamp: new java.util.Date().format("yyyy-MM-dd\'T\'HH:mm:ss.SSSZ"), 
+		status:'OK'
+	]
 
     def index() 
 	{
