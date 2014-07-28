75e590de147e214777ae3f61dd2e27c8840b0f80
Sandro Martini <sandro.martini@gmail.com>
2014-07-28 14:46:01 +0200

    small updates in test webapp, to make response data (fake) changing its timestamp (calculates now and returns it in the rest response)

--- test/webapp/grails-angularjs-resources-test24/grails-app/controllers/grails/angularjs/resources/test/RestSampleController.groovy
+++ test/webapp/grails-angularjs-resources-test24/grails-app/controllers/grails/angularjs/resources/test/RestSampleController.groovy
@@ -6,19 +6,19 @@ import grails.converters.JSON
 class RestSampleController 
 {
 
-	def sampleJSonResponse = [
-		id:1, 
-		controller:'RestSampleController', 
-		method:null, 
-		message:'Hello from Grails in JSON format', 
-		timestamp: new java.util.Date().format("yyyy-MM-dd HH:mm:ss.SSS"), 
-		// timestamp: new java.util.Date().format("yyyy-MM-dd\'T\'HH:mm:ss.SSSZ"), 
-		status:'OK'
-	]
-
     def index() 
 	{
 		log.info("index - params: $params")
+
+		def sampleJSonResponse = [
+			id:1, 
+			controller:'RestSampleController', 
+			method:null, 
+			message:'Hello from Grails in JSON format', 
+			timestamp: new java.util.Date().format("yyyy-MM-dd HH:mm:ss.SSS"), 
+			// timestamp: new java.util.Date().format("yyyy-MM-dd\'T\'HH:mm:ss.SSSZ"), 
+			status:'OK'
+		]
 
 		// render(view: "index")
 		// render Product.list() as JSON
