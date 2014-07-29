50b70b96691cc8fa3ec2a4b355465b3be9afbda1
Sandro Martini <sandro.martini@gmail.com>
2014-07-29 17:22:04 +0200

    update plugins to latest release

--- test/webapp/grails-angularjs-resources-test24/grails-app/conf/BuildConfig.groovy
+++ test/webapp/grails-angularjs-resources-test24/grails-app/conf/BuildConfig.groovy
@@ -76,7 +76,7 @@ grails.project.dependency.resolution = {
 
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.7'
-        compile ":asset-pipeline:1.9.3"
+        compile ":asset-pipeline:1.9.4"
 
         runtime ":hibernate4:4.3.5.4"
         runtime ":database-migration:1.4.0"
