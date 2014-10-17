e0aa7d3fc67e3e076878e810a02af0dd6a798630
Sandro Martini <sandro.martini@gmail.com>
2014-10-17 17:36:45 +0200

    update AngularJS to 1.3.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -19,7 +19,7 @@ grails.project.work.dir = "target"
 grails.project.source.level = 1.6
 grails.project.target.level = 1.6
 
-def angularJSVersion = '1.2.26'
+def angularJSVersion = '1.3.0'
 
 grails.project.dependency.resolution = {
 
