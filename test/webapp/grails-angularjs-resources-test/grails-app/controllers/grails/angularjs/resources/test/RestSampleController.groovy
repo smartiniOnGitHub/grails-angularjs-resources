97cdd1ab8894222ad83a4037d5260700ae315fc2
Sandro Martini <sandro.martini@gmail.com>
2014-10-27 18:32:55 +0100

    small changes in RestSampleController, to be a little more general (and a little more Grails-like)

--- test/webapp/grails-angularjs-resources-test/grails-app/controllers/grails/angularjs/resources/test/RestSampleController.groovy
+++ test/webapp/grails-angularjs-resources-test/grails-app/controllers/grails/angularjs/resources/test/RestSampleController.groovy
@@ -1,18 +1,19 @@
 package grails.angularjs.resources.test
 
 import grails.converters.JSON
+import grails.converters.XML
 
 
 class RestSampleController 
 {
 
-	def sampleJSonResponse = [
+	def sampleDataResponse = [
 		id:1, 
-		controller:'RestSampleController', 
-		method:null, 
+		// controller:'RestSampleController', 
+		// action:'index', // set a default value
 		message:'Hello from Grails in JSON format', 
+		initialTimestamp: new java.util.Date().format("yyyy-MM-dd\'T\'HH:mm:ss.SSSZ"), 
 		timestamp: new java.util.Date().format("yyyy-MM-dd HH:mm:ss.SSS"), 
-		// timestamp: new java.util.Date().format("yyyy-MM-dd\'T\'HH:mm:ss.SSSZ"), 
 		status:'OK'
 	]
 
@@ -20,9 +21,19 @@ class RestSampleController
 	{
 		log.info("index - params: $params")
 
-		// render(view: "index")
-		// render Product.list() as JSON
-		render sampleJSonResponse as JSON
+		// test, update timestamp with current ...
+		sampleDataResponse['timestamp'] = new java.util.Date().format('yyyy-MM-dd HH:mm:ss.SSS')
+		sampleDataResponse['controller'] = controllerName
+		sampleDataResponse['action'] = actionName
+
+		// render sampleDataResponse as JSON
+		// more generic
+		return withFormat {
+			// html { render "<p>Hello from $controllerName.$actionName</p>" }
+			// js   { render "alert('Hello from $controllerName.$actionName')" }
+			json { render sampleDataResponse as JSON }
+			xml  { render sampleDataResponse as XML  }
+		} ?: response.sendError(response.SC_UNSUPPORTED_MEDIA_TYPE)
 	}
 
 }
--- test/webapp/grails-angularjs-resources-test24/grails-app/controllers/grails/angularjs/resources/test/RestSampleController.groovy
+++ test/webapp/grails-angularjs-resources-test24/grails-app/controllers/grails/angularjs/resources/test/RestSampleController.groovy
@@ -1,28 +1,39 @@
 package grails.angularjs.resources.test
 
 import grails.converters.JSON
+import grails.converters.XML
 
 
 class RestSampleController 
 {
 
+	def sampleDataResponse = [
+		id:1, 
+		// controller:'RestSampleController', 
+		// action:'index', // set a default value
+		message:'Hello from Grails in JSON format', 
+		initialTimestamp: new java.util.Date().format("yyyy-MM-dd\'T\'HH:mm:ss.SSSZ"), 
+		timestamp: new java.util.Date().format("yyyy-MM-dd HH:mm:ss.SSS"), 
+		status:'OK'
+	]
+
     def index() 
 	{
 		log.info("index - params: $params")
 
-		def sampleJSonResponse = [
-			id:1, 
-			controller:'RestSampleController', 
-			method:null, 
-			message:'Hello from Grails in JSON format', 
-			timestamp: new java.util.Date().format("yyyy-MM-dd HH:mm:ss.SSS"), 
-			// timestamp: new java.util.Date().format("yyyy-MM-dd\'T\'HH:mm:ss.SSSZ"), 
-			status:'OK'
-		]
+		// test, update timestamp with current ...
+		sampleDataResponse['timestamp'] = new java.util.Date().format('yyyy-MM-dd HH:mm:ss.SSS')
+		sampleDataResponse['controller'] = controllerName
+		sampleDataResponse['action'] = actionName
 
-		// render(view: "index")
-		// render Product.list() as JSON
-		render sampleJSonResponse as JSON
+		// render sampleDataResponse as JSON
+		// more generic
+		return withFormat {
+			// html { render "<p>Hello from $controllerName.$actionName</p>" }
+			// js   { render "alert('Hello from $controllerName.$actionName')" }
+			json { render sampleDataResponse as JSON }
+			xml  { render sampleDataResponse as XML  }
+		} ?: response.sendError(response.SC_UNSUPPORTED_MEDIA_TYPE)
 	}
 
 }
