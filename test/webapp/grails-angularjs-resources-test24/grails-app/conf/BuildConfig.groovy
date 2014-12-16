6baff444edcfaa52e393257b2b5de755690d5d22
Sandro Martini <sandro.martini@gmail.com>
2014-12-16 18:29:13 +0100

    update dependencies to latest stable asset-pipeline (2.0.19) and related plugins

--- test/webapp/grails-angularjs-resources-test24/grails-app/conf/BuildConfig.groovy
+++ test/webapp/grails-angularjs-resources-test24/grails-app/conf/BuildConfig.groovy
@@ -76,7 +76,7 @@ grails.project.dependency.resolution = {
 
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
-        compile ":asset-pipeline:1.9.9"
+        compile ":asset-pipeline:2.0.19"
 
         runtime ":hibernate4:4.3.6.1"
         runtime ":database-migration:1.4.0"
@@ -85,7 +85,7 @@ grails.project.dependency.resolution = {
         runtime ":resources:1.2.8"
 
         // Uncomment these to enable additional asset-pipeline capabilities
-        //compile ":sass-asset-pipeline:1.9.0"
+        //compile ":sass-asset-pipeline:1.9.2"
         //compile ":less-asset-pipeline:1.10.0"
         //compile ":coffee-asset-pipeline:1.9.0"
     }
